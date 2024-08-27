882cf919b8361be3710f10c2f0ceab8138be14b6
enkyuan <yuan_enkang@outlook.com>
2024-08-27 17:05:48 -0500

    added .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://enkyuan.github.io",
+  base: "/enkyuan.github.io",
   integrations: [mdx(), sitemap(), tailwind()],
 });
