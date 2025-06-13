da0a7afc4f43406f2cd4726cb5e8a0d3c273e2a6
enkyuan <yuan_enkang@outlook.com>
2025-06-13 03:00:00 -0500

    chore: more metadata

---
+++ src/routes/+layout.ts
@@ -0,0 +1,23 @@
+import { base } from '$app/paths';
+
+export const load = () => {
+    return {
+        metadata: {
+            title: 'Enkang Yuan',
+            description: 'my corner of the internet',
+            openGraph: {
+                title: 'Enkang Yuan',
+                description: 'my corner of the internet',
+                url: `${base}/`,
+                type: 'website',
+                image: `${base}/favicon/favicon-96x96.png`,
+            },
+            twitter: {
+                card: 'summary',
+                title: 'Enkang Yuan',
+                description: 'my corner of the internet',
+                image: `${base}/favicon/favicon-96x96.png`,
+            }
+        }
+    };
+}; 
