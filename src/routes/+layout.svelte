960e8c4416335e97ea9b9314ed3eee1e14b0201a
enkyuan <yuan_enkang@outlook.com>
2025-06-22 13:12:31 -0500

    chore: removed .layout in styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,31 @@
 <script lang="ts">
-import "../app.css";
-import { page } from "$app/state";
-import Sidebar from "../lib/components/navigation/sidebar.svelte";
-import Hamburger from "../lib/components/navigation/hamburger.svelte";
+    import "../app.css";
+    import { page } from "$app/state";
+    import Sidebar from "../lib/components/navigation/sidebar.svelte";
+    import Hamburger from "../lib/components/navigation/hamburger.svelte";
 
-export const prerender = true;
+    export const prerender = true;
 </script>
 
 <div class="desktop-only">
-	<Sidebar pathname={page.url.pathname} />
+    <Sidebar pathname={page.url.pathname} />
 </div>
 
 <div class="mobile-only">
-	<Hamburger />
+    <Hamburger />
 </div>
 
 <main>
-	<slot />
+    <slot />
 </main>
 
 <style>
-	.layout {
-		display: flex;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		width: 100vw;
-		max-width: 100vw;
-		box-sizing: border-box;
-	}
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        width: 100vw;
+        max-width: 100vw;
+        box-sizing: border-box;
+    }
 </style>
