ed6cd2f2b70349667bda9d094fd672437af1d0b5
袁恩康 <yuan_enkang@outlook.com>
2026-01-08 16:47:11 -0600

    chore: modified socials

--- src/lib/constants/socials.ts
+++ src/lib/constants/socials.ts
@@ -1,5 +1,5 @@
 export const socials = [
 	{ text: "Twitter", href: "https://twitter.com/enkyuan" },
 	{ text: "GitHub", href: "https://github.com/enkyuan" },
-	{ text: "LinkedIn", href: "https://linkedin.com/in/enkyuan" },
+	{ text: "Email", href: "mailto:yuan.enkng@gmail.com" },
 ];
